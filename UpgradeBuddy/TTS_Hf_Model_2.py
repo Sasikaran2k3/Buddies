616cf97030753e7388ec4d2aa00cd4883c1b3657
Sasi <sasi@localhost.localdomain>
2024-01-19 21:38:38 +0530

    With Template

---
+++ UpgradeBuddy/TTS_Hf_Model_2.py
@@ -0,0 +1,16 @@
+import torch
+from TTS.api import TTS
+import os
+import datetime
+
+date = "".join(str(datetime.date.today()).split("-"))
+f = open(os.path.dirname(__file__) + "//Data//" + date + "_script.txt", "r")
+content = f.readlines()
+stripped_content = ""
+for i in content:
+    stripped_content += i.strip() + "\n"
+    stripped_content = stripped_content + ". Hey Buddy, share with someone who is eager for this info! Stay updated by following Upgrade Buddy, hit like, and drop 'buddy' in the comments for more details!"
+print(stripped_content)
+tts = TTS(model_name="tts_models/en/vctk/vits", progress_bar=False)
+path = f"{os.path.dirname(__file__)}/Data/{date}.wav"
+tts.tts_to_file(text=stripped_content, file_path=path,speaker="p302")
