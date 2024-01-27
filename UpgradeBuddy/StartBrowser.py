dc50bf9e84602704f0f40f33aa286ce86b927ec9
Sasi <sasikaran2k3@gmail.com>
2024-01-27 18:29:41 +0530

    Both Buddies with GPT

--- UpgradeBuddy/StartBrowser.py
+++ UpgradeBuddy/StartBrowser.py
@@ -1,5 +1,4 @@
 import os
-from subprocess import call
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -9,15 +8,15 @@ def Start_Lap(browser_name="UpgradeBuddy"):
     # Initialization of web Driver
     opt = Options()
     # This option is used to verify the action part without starting from beginning
-    opt.add_experimental_option(    'debuggerAddress',"localhost:1135")
-    # CMD prompt is google-chrome --remote-debugging-port=1135 --user-data-dir="/home/sasi/PycharmProjects/EntertainBuddy/EntertainBuddy"
+    opt.add_experimental_option('debuggerAddress',"localhost:1135")
+    # CMD prompt is google-chrome --remote-debugging-port=1135 --user-data-dir="/home/sasi/PycharmProjects/UpgradeBuddy/UpgradeBuddy"
     path_of_browser = os.path.dirname(__file__) + '/%s' % browser_name
-    print(r'--user-data-dir=%s'%path_of_browser)
-    opt.add_argument("--remote-debugging-port=1135")
-    opt.add_argument(r'--user-data-dir=%s'%path_of_browser)
+    print(path_of_browser)
     os.system("gnome-terminal -e 'google-chrome --remote-debugging-port=1135 --user-data-dir=\"/home/sasi/PycharmProjects/UpgradeBuddy/UpgradeBuddy\"'")
+    opt.add_argument(r'--user-data-dir=%s'%path_of_browser)
     services = Service(executable_path=os.path.dirname(__file__) + "/chromedriver")
     browser = Chrome(service=services, options=opt)
+    browser.minimize_window()
     browser.maximize_window()
     browser.implicitly_wait(15)
     return browser
